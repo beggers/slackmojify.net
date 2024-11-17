198af2f649c59f4ed4444c5efa2ed58fb6e2b486
Ben Eggers <ben@beneggers.com>
2024-11-17 10:33:59 -0800

    YOOOO it works on my jpg

--- src/components/EffectsControl.tsx
+++ src/components/EffectsControl.tsx
@@ -15,15 +15,16 @@ function EffectsControl({ image, setProcessedImage }: EffectsControlProps) {
         console.log('Received valid original image:', image);
 
         const image_buf = await image.arrayBuffer();
+        const image_blob = new Blob([image_buf], { type: 'image/jpg' });
 
         try {
             console.log('Removing background...');
-            const result = await removeBackground(image_buf);
+            const result = await removeBackground(image_blob);
             if (result) {
                 console.log('Background removal successful');
                 setProcessedImage(result);
             } else {
-                console.error('Background removal failed: Result is undefined or does not contain a canvas');
+                console.error('Background removal failed: Result is undefined or null? ', result);
             }
         } catch (error) {
             console.error('Error during background removal:', error);
