b72a4548f1ea97dfef523fcfe055d60ff52d3105
Ben Eggers <ben@beneggers.com>
2024-11-20 15:03:55 -0800

    it works

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -44,12 +44,33 @@ resource "aws_s3_bucket_policy" "main" {
   depends_on = [aws_s3_bucket_public_access_block.main]
 }
 
+locals {
+  mime_types = {
+    html = "text/html"
+    css  = "text/css"
+    js   = "application/javascript"
+    png  = "image/png"
+    jpg  = "image/jpeg"
+    jpeg = "image/jpeg"
+    gif  = "image/gif"
+  }
+}
+
 resource "aws_s3_object" "website_files" {
   for_each = fileset(var.build_directory, "**/*")
 
-  bucket = aws_s3_bucket.main.bucket
-  key    = each.value
-  source = "${var.build_directory}/${each.value}"
-  etag   = filemd5("${var.build_directory}/${each.value}")
-  acl    = "public-read"
+  bucket      = aws_s3_bucket.main.bucket
+  key         = each.value
+  source      = "${var.build_directory}/${each.value}"
+  source_hash = filemd5("${var.build_directory}/${each.value}")
+  etag        = filemd5("${var.build_directory}/${each.value}")
+  acl         = "public-read"
+  content_type = lookup(
+    local.mime_types,
+    element(
+      split(".", each.value),
+      length(split(".", each.value)) - 1
+    ),
+    "application/octet-stream"
+  )
 }
