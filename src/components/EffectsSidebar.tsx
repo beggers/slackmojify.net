c9b19462e9d50e79ba0d78a01a9ee6d6e328a8d4
Ben Eggers <ben@beneggers.com>
2024-11-21 11:05:59 -0800

    make it happy

--- src/components/EffectsSidebar.tsx
+++ src/components/EffectsSidebar.tsx
@@ -4,14 +4,6 @@ interface EffectsSidebarProps {
 }
 
 function EffectsSidebar({ image, setProcessedImage }: EffectsSidebarProps) {
-    const handleEffectClick = (effect: string) => {
-        if (!image) {
-            console.error('No image found to apply effect');
-            return;
-        }
-        console.log(`Applying effect: ${effect}`);
-    };
-
     return (
         <div className="effects-sidebar">
             <h3>Add overlay images</h3>
